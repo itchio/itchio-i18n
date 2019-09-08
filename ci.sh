a58847a757bedda4d5eaeb9c81b85d72b59ad6fe
leaf corcoran <leafot@gmail.com>
2019-09-07 19:20:54 -0700

    fix ci script for latest docker image

--- ci.sh
+++ ci.sh
@@ -4,11 +4,11 @@ set -o pipefail
 set -o xtrace
 
 # setup lua
-luarocks-5.1 install moonscript
-luarocks-5.1 install lua-cjson
-luarocks-5.1 install tableshape
-luarocks-5.1 install luafilesystem
-eval $(luarocks-5.1 path)
+luarocks --lua-version=5.1 install moonscript
+luarocks --lua-version=5.1 install lua-cjson
+luarocks --lua-version=5.1 install tableshape
+luarocks --lua-version=5.1 install luafilesystem
+eval $(luarocks --lua-version=5.1 path)
 
 cat $(which busted) | sed 's/\/usr\/bin\/lua5\.1/\/usr\/bin\/luajit/' > busted
 chmod +x busted
